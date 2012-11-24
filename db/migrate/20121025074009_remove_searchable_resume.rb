812e98fa7b40a067644abcec177c5b9bcd65fbb2
klairvoyant infosolutions <contact@klairvoyant.com>
2012-11-24 16:18:25 +0530

    nov 24

--- db/migrate/20121025074009_remove_searchable_resume.rb
+++
@@ -1,8 +0,0 @@
-class RemoveSearchableResume < ActiveRecord::Migration
-  def up
-    remove_column :resumes, :searchable
-  end
-
-  def down
-  end
-end
--- db/migrate/20121115121324_remove_resumes_table.rb
+++
@@ -1,8 +0,0 @@
-class RemoveResumesTable < ActiveRecord::Migration
-  def up
-    drop_table :resumes
-  end
-
-  def down
-  end
-end
