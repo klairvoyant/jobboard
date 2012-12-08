a969f50c595da751a8483229ec76ad9546f4653a
klairvoyant infosolutions <contact@klairvoyant.com>
2012-12-08 15:48:10 +0530

    DEC  8 migration add default data

--- db/migrate/20121208100225_add_some_data.rb
+++ db/migrate/20121208100225_add_some_data.rb
@@ -1,8 +1,8 @@
 class AddSomeData < ActiveRecord::Migration
   def up
-    execute("insert into interest_areas(interest) values('IT');")
-    execute("insert into job_types(job_type) values('General Manager');")
-    execute("insert into salary_ranges(salary_range) values('Above 2000');")
+    execute("insert into interest_areas(interest,created_at,updated_at) values('IT','2012-11-14 13:34:24','2012-11-14 13:34:24');")
+    execute("insert into job_types(job_type,created_at,updated_at) values('General Manager','2012-11-14 13:34:24','2012-11-14 13:34:24');")
+    execute("insert into salary_ranges(salary_range,created_at,updated_at) values('Above 2000','2012-11-14 13:34:24','2012-11-14 13:34:24');")
   end
 
   def down
